cb4c34ae2522889747086ec51fd6cfd73426783a
jtmunoz <jordantmunoz@gmail.com>
2016-06-21 11:12:03 -0700

    Add 2nd suspetct to zom

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -5,35 +5,36 @@
 # committed the crime, add additional calls to prove it.
 
 puts "iNvEsTiGaTiOn".swapcase
-# => “InVeStIgAtIoN”
+puts "InVeStIgAtIoN"
 
 puts "zom".replace "zoom"
-# => “zoom”
+puts "zom".insert(1, 'o')
+puts "zoom"
 
 puts "enhance".center(16)
-# => "    enhance    "
+puts "    enhance    "
 
 puts "Stop! You’re under arrest!".upcase
-#=> "STOP! YOU’RE UNDER ARREST!"
+puts "STOP! YOU’RE UNDER ARREST!"
 
 puts "the usual".insert(-1, ' supsects')
-#=> "the usual suspects"
+puts "the usual suspects"
 
 puts " suspects".insert(0,'the usual')
-# => "the usual suspects"
+puts "the usual suspects"
 
 puts "The case of the disappearing last letter".chop
- #=> "The case of the disappearing last lette"
+puts "The case of the disappearing last lette"
 
 puts "The mystery of the missing first letter".delete "T"
-# => "he mystery of the missing first letter"
+puts "he mystery of the missing first letter"
 
 puts "Elementary,    my   dear        Watson!".squeeze
-# => "Elementary, my dear Watson!"
+puts "Elementary, my dear Watson!"
 
 puts "z".getbyte(0)
-# => 122 
+puts 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
 puts "How many times does the letter 'a' appear in this string?".count "a"
-# => 4
+puts 4
