8394cb700d6720128fb53780823e3c9f4582d632
jtmunoz <jordantmunoz@gmail.com>
2016-06-23 13:05:41 -0700

    Trying to fix errors

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,4 +1,9 @@
 
+#At the top of your file, add a method that takes three items as parameters and returns an array 
+#of those items. So build_array(1, "two", nil) would return [1, "two", nil]. 
+#This won't take much code, but the syntax might feel a bit odd. 
+#At the bottom of the file, call the method to show that it works.
+
 def add(x, y, z)
 #	x + y + z
 	build = Array.new
@@ -8,17 +13,27 @@ end
 #At the top of your file, add a method that takes an array and an item as parameters
 #, and returns the array with the item added.
 
-def ary_item(array, item)
-	[] << x	
-	array = Array.new
-	array << item
+def ary_item(generic_array, item)
+	item = ""
+	generic_array << item
+	#generic_array = ["coffee", "cream", "sugar"]
 
-#	[x, y, z] << j
+	
+
 end	
 
 # So add_to_array([], "a") would return ["a"], 
 #and add_to_array(["a", "b", "c", 1, 2], 3) would return ["a", "b", "c", 1, 2, 3]. 
 #Print a few test calls of the method.
+
+#Initialize an empty array and store it in a variable (you can choose the name). Print the variable using p.
+#Add five items to your array. Print the array.
+#Delete the item at index 2. Print the array.
+#Insert a new item at index 2. Print the array.
+#Remove the first item of the array without having to refer to its index. Print the array.
+#Ask the array whether it includes a certain item. Print the result of this method call (labeled so it prints as more than just an isolated boolean value without any context.)
+#Initialize another array that already has a few items in it.
+#Add the two arrays together and store them in a new variable. Print the new array.
 
 espn = Array.new
 #p espn
@@ -41,4 +56,6 @@ new_variable = nbc + espn
 #p new_variable
 
 puts add("apple","carrot","potato")
-puts ary_item(#{build, "bread")
+p ary_item([], "espresso")
+
+
