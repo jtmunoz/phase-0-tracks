b38ec3b5406b3541dc0a79006fa3a1138e53c951
jtmunoz <jordantmunoz@gmail.com>
2016-06-25 16:24:04 -0700

    Add itteration .each through meals hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,3 +5,18 @@ def greeting
 end
 
 greeting { |species1, species2|  puts "greetings #{species1} and #{species2}"}
+
+meals = {
+	breakfast: "pancakes",
+	lunch: "sandwich",
+	dinner: "lobster bisque"
+}
+
+puts "We love our food"
+
+meals.each do |meal, food|
+	puts "For #{meal} we ate #{food}"
+end
+
+calories = [1750, 750, 1500]
+
