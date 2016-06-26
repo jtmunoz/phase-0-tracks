a6b0d9262b4ad8f08c0811956d92a23de98453f1
jtmunoz <jordantmunoz@gmail.com>
2016-06-26 12:47:19 -0700

    Initial commit

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,7 @@
+#design and build a nested data structure to represent a real-world construct. 
+#You can choose the construct: 
+# Build something that will use a mix of hashes and arrays.
+
+#print a few individual pieces of deeply nested dat from the structure
+#showing that you know how to use multiple indexes or hash keys (or both) 
+# access nested items. Try to demonstrate a few different types of access.
