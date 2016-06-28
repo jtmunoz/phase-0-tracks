73f4b783750c630ff76cdd0d288771697b995cf3
jtmunoz <jordantmunoz@gmail.com>
2016-06-27 20:41:28 -0700

    Add pseudocode

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,32 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # [fill in any steps here]
+  # grocery list
+  # default quantity 4
+  # 
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: [array]
+
+
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: create a hash, give each item would be a key, optional quantity
+# output: the hash would be the items and the quantities
+
+# Method to remove an item from the list
+# input: remove_item [:key]
+# steps: delete
+# output: the hash minus the removed key
+
+# Method to update the quantity of an item
+# input: updating key [:item_name]
+# steps: call one of the keys in the hash
+# output: a new updated value of an item
+
+# Method to print a list and make it look pretty
+# input: shopping list[:item] = "value" within our shopping array
+# steps: puts " [:key] and value"
+# output: our list nice and pretty
