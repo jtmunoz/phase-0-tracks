4bd99509623ed980f1e5d001f871782efa879d4c
jtmunoz <jordantmunoz@gmail.com>
2016-06-29 16:11:18 -0700

    Add Kitty class, and two instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,27 +1,27 @@
 class Puppy
+      def initialize
+        puts "Initializing new puppy instances"
+     	end
+  
+  
+    def fetch(toy)
+      puts "I brought back the #{toy}!"
+      toy
+    end
 
-def initialize(name) 
-	@name = "Bear"
-end
+    def speak(x)
+      x.times  { puts "Woof" }
+    end
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+    def roll_over
 
-  def speak(x)
-  	x.times  { puts "Woof" }
-  end
+      puts "*roll over*"
+    end
 
-  def roll_over(item)
-  	puts "#{@name}"
-  	puts "*roll over*"
-  end
-
-  def dog_year(human_year)
-  	dog_year = human_year.to_i * 7
-  	puts "Human year #{human_year} is dog year #{dog_year}"
-  end
+    def dog_year(human_year)
+        dog_year = human_year.to_i * 7
+        puts" Human year #{human_year} is dog year #{dog_year}"
+    end
 
 end
 
@@ -31,7 +31,22 @@ spot.fetch("shoe")
 
 spot.speak(3)
 
-spot.roll_over(@name)
+spot.roll_over
 
 spot.dog_year(10)
+#store = Puppy.new
+#jordan= Puppy.new
 
+class Kitty
+	def initialize
+		puts "Meow, Meow, Meow"
+	end
+
+	def milk
+		puts "I like milk"
+	end
+
+	def dogs
+		puts "I don't like dogs"
+	end
+end
