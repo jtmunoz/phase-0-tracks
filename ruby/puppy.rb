0128ef3989960bdbcc6ac920c16832282eb488f5
jtmunoz <jordantmunoz@gmail.com>
2016-06-29 14:52:51 -0700

    fetch an roll 0ver method

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -24,23 +24,40 @@
 
 class Lycanthrope
 	
+	#method: high_five
+	def high_five
+		puts "HI 5"
+	end
+	
+	#method: fetch retrieves an item
+	def fetch
+		puts "What can I fetch?"
+		answer = gets.chomp
+		x = answer
+		puts "I brought you back #{x}"
+	end
+
+	#method: roll over re
+	def roll_over
+		puts "Look I am upside down, scratch my belly"
+	end
+
 	#method: play_dead
 	def play_dead
 		puts "(x) (x)"
+		puts "   0   "
+		puts "  ---  "
 	end
-
-	#method: high_five
-	def high_five
-	puts "HI 5"
-	end
-
-
+	
 end
 
 
 cloud = Lycanthrope.new
 #p cloud.class
 
-puts cloud.play_dead
-puts cloud.high_five
 
+puts cloud.high_five
+puts cloud.fetch
+puts cloud.roll_over
+
+puts cloud.play_dead
