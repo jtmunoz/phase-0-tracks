205f2acdbab3474a8d7ca4ee88cde1b720b3a894
jtmunoz <jordantmunoz@gmail.com>
2016-06-29 15:58:36 -0700

    Add methods to puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,9 @@
 class Puppy
 
+def initialize(name) 
+	@name = "Bear"
+end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -9,9 +13,25 @@ class Puppy
   	x.times  { puts "Woof" }
   end
 
+  def roll_over(item)
+  	puts "#{@name}"
+  	puts "*roll over*"
+  end
+
+  def dog_year(human_year)
+  	dog_year = human_year.to_i * 7
+  	puts "Human year #{human_year} is dog year #{dog_year}"
+  end
+
 end
 
 spot = Puppy.new
+
 spot.fetch("shoe")
 
 spot.speak(3)
+
+spot.roll_over(@name)
+
+spot.dog_year(10)
+
