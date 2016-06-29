6841acc1f1838f96ecd89b47f6be5c315f4792f1
jtmunoz <jordantmunoz@gmail.com>
2016-06-29 14:31:47 -0700

    Add play_dead and high_five method

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,15 +1,46 @@
-#---Species---
-# lycanthropus awesomeness
+#---Species--- (CLASS)
+#lycanthrope
 
-#---Characteristics---
+#---member--- (INSTANCE)
+#cloud
+
+#---Characteristics--- (ATTRIBUTES)
 # Fangs: yes
 # Friendly: varies
 # Eyes: 2
 # Name: varies
 # Color: varies
 
-#---Behavior---
+#---Behavior--- (METHODS)
 # Howl
 # Sprint
 # Destroy
 # #Hunt
+
+#instance methods
+#fetch, roll over, or offer a high five
+
+
+
+class Lycanthrope
+	
+	#method: play_dead
+	def play_dead
+		puts "(x) (x)"
+	end
+
+	#method: high_five
+	def high_five
+	puts "HI 5"
+	end
+
+
+end
+
+
+cloud = Lycanthrope.new
+#p cloud.class
+
+puts cloud.play_dead
+puts cloud.high_five
+
