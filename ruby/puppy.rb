b3c7b6c8890b7f45ef203f8a296b8e01f8eb3c77
jtmunoz <jordantmunoz@gmail.com>
2016-06-29 13:40:58 -0700

    Initial Commit, Comments for new clss

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+#---Species---
+# lycanthropus awesomeness
+
+#---Characteristics---
+# Fangs: yes
+# Friendly: varies
+# Eyes: 2
+# Name: varies
+# Color: varies
+
+#---Behavior---
+# Howl
+# Sprint
+# Destroy
+# #Hunt
