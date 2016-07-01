a5976e39db35730b49891355a02888a787fda218
jtmunoz <jordantmunoz@gmail.com>
2016-06-30 20:04:44 -0700

    Initial commit and Speak method

---
+++ ruby/santa.rb
@@ -0,0 +1,13 @@
+class Santa
+#A speak method that will print "Ho, ho, ho! Haaaappy holidays!" 
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+#An eat_milk_and_cookies method that takes a cookie type (example: "snickerdoodle") as a parameter and prints "That was a good <type of cookie here>!" 
+#An initialize method that prints "Initializing Santa instance ...".
+#Add a bit of code below your class declaration to check that you're able to initialize a Santa instance and call its methods.
+end
+
+nick = Santa.new
+#p nick.speak
+
