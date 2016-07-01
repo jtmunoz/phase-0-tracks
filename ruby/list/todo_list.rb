928aed0e623d6b5dbe57302ff4217dbe8f38762b
jtmunoz <jordantmunoz@gmail.com>
2016-07-01 14:19:17 -0700

    Initial Commit

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,9 @@
+# maintains an internal array attribute and 
+# provides methods that allow the array to be 
+#accessed and updated in various ways.
+
+class TodoList
+	def initialize(list)
+		@list = list
+	end
+end
