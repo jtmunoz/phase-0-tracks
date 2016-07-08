ffd2431e4c76c918477d6359b9e454496bb40832
jtmunoz <jordantmunoz@gmail.com>
2016-07-08 08:16:21 -0700

    Add initial commit

---
+++ ruby/solo.rb
@@ -0,0 +1,6 @@
+#at least three attributes (using at least two data types) whose values 
+#will vary from instance to instance (example: age)
+#at least three methods, at least one of which takes an argument
+
+#use attr_reader and attr_accessor
+#Use an initialize method that sets at least one attribute
