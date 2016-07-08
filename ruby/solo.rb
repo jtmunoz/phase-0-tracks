ccd5a461e2b4e1a170b01331634dc191e28382c0
jtmunoz <jordantmunoz@gmail.com>
2016-07-08 11:36:58 -0700

    Add attr accessor name, initialize method, about method

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -4,3 +4,25 @@
 
 #use attr_reader and attr_accessor
 #Use an initialize method that sets at least one attribute
+
+# a drink class
+class Drink
+	attr_accessor :name
+	def initialize
+		#puts "What can I get started for you?"
+		@name = name
+	end
+
+	def about
+		puts " I have a drink for #{@name}"
+	end
+end
+
+puts "Hello I am Barista: Hal 9000, Can I get a drink started for you?"
+puts "Can I get your name?"
+name = gets.chomp
+@name = name
+
+new_drink = Drink.new
+new_drink.name = name
+new_drink.about
