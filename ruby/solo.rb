2788c74bd186c0ada8b1a659cbd9ff9ba0d9d64b
jtmunoz <jordantmunoz@gmail.com>
2016-07-09 11:34:42 -0700

    Add coffee choice method

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -8,7 +8,7 @@
 # an order class
 class Order
 
-	attr_reader 
+	attr_reader  :beans
 
 	attr_accessor :name, :number_of_drinks
 	
@@ -17,6 +17,7 @@ class Order
 		#puts "What can I get started for you?"
 		@name = name
 		@number_of_drinks = 0
+		@beans = ["Pike", "Veranda", "Sumatra", "Decaf Espresso"]
 	end
 
 	#gets name for order
@@ -28,28 +29,43 @@ class Order
 
 	#method where input x is number of drinks
 	def how_many
-		puts "#{@name} how many drinks can I get you today"
+		puts "#{@name} how many drinks can I get you today?"
 		@number_of_drinks = gets.chomp
 	end
 
-	#redeem
+	#redeem_method should subtract one from @number of drinks
+	def free_one
+		puts "Congrats on being the very first customer. Let me help you out."
+		drinks = @number_of_drinks.to_i - 1
+		@number_of_drinks = drinks
+		puts "Now you only have to pay for #{drinks}."
+	end
+
+	#user decides favorite coffee from array
+	def coffee_choice
+		puts "What is your favorite coffee?"
+		p @beans
+		favorite = gets.chomp
+		#@beans.delete_if{|beans| beans != favorite}
+		#@beans
+
+	end
+
 
 	#prints out information
 	def about
-		puts "I have a #{@number_of_drinks} drinks for #{@name}"
+		puts "I have a #{@number_of_drinks} drinks for #{@name}."
 	end
 
 
 end
 
 
-#puts "Hello I am Barista: Hal 9000, Can I get a drink started for you?"
-#puts "Can I get your name?"
-#@name = gets.chomp
-#@name = name
-
 new_drink = Order.new
 new_drink.order_name
-
 new_drink.how_many
+new_drink.free_one
+new_drink.coffee_choice
+
 new_drink.about
+p new_drink
