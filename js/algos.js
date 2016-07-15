9e0932672a0aa1df2f636c49af73fb67680088e5
jtmunoz <jordantmunoz@gmail.com>
2016-07-15 15:14:29 -0700

    Initial Commit

---
+++ js/algos.js
@@ -0,0 +1,6 @@
+//wite a function that takes an array of words or phrases 
+//and returns the longest word or phrase in the array.
+// So if we gave your function the array of 
+//["long phrase","longest phrase","longer phrase"], 
+//it would return "longest phrase"
+
