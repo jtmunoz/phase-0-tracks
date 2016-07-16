d895c1693bf4e1494b70897b04d8a06e266a67f7
jtmunoz <jordantmunoz@gmail.com>
2016-07-16 13:43:46 -0700

    Add object Car

--- js/data_structures.js
+++ js/data_structures.js
@@ -11,3 +11,18 @@ for(var i = 0; i < horseName.length; i++){
 };
 
 console.log(nameColors);
+
+function Car(type, year, isHybrid){
+	console.log("Our new car:", this);
+	this.type = type;
+	this.year = year;
+	this.isHybrid = isHybrid;
+
+	this.drive = function(){
+		console.log("Vroom, Vroom");
+	};
+
+} 
+
+var newCar = new Car("toyota", 1986, false);
+console.log(newCar);
